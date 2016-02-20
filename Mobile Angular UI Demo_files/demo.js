f77da10018724a53814c8e8760e866a9644e0911
OverAchiever <eecybermonk@gmail.com>
2016-02-20 15:44:48 +0200

    progress:

--- Mobile Angular UI Demo_files/demo.js
+++ Mobile Angular UI Demo_files/demo.js
@@ -255,6 +255,26 @@ app.directive('dragMe', ['$drag', function($drag){
 //
 app.controller('MainController', function($rootScope, $scope){
 
+  function initMap() {
+
+    var myLatLng = {lat: -25.363, lng: 131.044};
+
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      scrollwheel: false,
+      zoom: 4
+    });
+
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+      map: map,
+      position: myLatLng,
+      title: 'Hello World!'
+    });
+  }
+  initMap();
+
   $scope.swiped = function(direction) {
     alert('Swiped ' + direction);
   };
